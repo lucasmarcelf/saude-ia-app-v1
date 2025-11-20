3550b28f48676216c405e89644862cb050576158
Lucas Marcel <lucasmarcel.fe@gmail.com>
2025-11-20 16:49:59 -0300

    modified libre_client

--- monitoring/libre_client.py
+++ monitoring/libre_client.py
@@ -5,13 +5,13 @@ from pylibrelinkup import PyLibreLinkUp, APIUrl
 def get_libre_client() -> PyLibreLinkUp:
     email = settings.LIBRELINKUP_EMAIL
     password = settings.LIBRELINKUP_PASSWORD
-    region_env = settings.LIBRELINKUP_API_URL
+    # region_env = settings.LIBRELINKUP_API_URL
 
-    api = APIUrl.from_string(region_env.upper())
+    # api = APIUrl.from_string(region_env.upper())
 
     if not email or not password:
         raise RuntimeError("LIBRE_EMAIL ou LIBRE_PASSWORD não configurados no .env")
 
-    client = PyLibreLinkUp(email=email, password=password, api_url=api)
+    client = PyLibreLinkUp(email=email, password=password, api_url=APIUrl.LA)
     client.authenticate()
     return client
